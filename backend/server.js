99aca3e365388bc4e0dad4c35a1c5d26af61d807
Bruno <brunocostascuciato009@gmail.com>
2025-09-02 09:02:53 -0300

    finalizando o codigo do server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,40 @@ const app = express();
 // DEFININDO A PORTA DE APLICAÇÃO
 const port = 5001;
 
+// CONFIGURAR O EXPRESS PARA REQUISIÇÕES EM JSON
+app.use(express.json());
+
+// HABILITA O COR PRA ACEITAR REQUISICOES
+app.use(cors());
+
+// TABELA DE PREÇOS
+const precos ={
+    bicicleta: 0.80,
+    carro: 0.95,
+    drone: 1.20
+}
+
+// CRIANDO A ROTA DA APLICAÇÃO
+app.post("/calcularfrete",(req,res)=>{
+    // desustruturação para extrair as variaveis da aplicação
+    const{distancia,tipoTransporte}= req.body;
+
+    if(distancia === undefined || tipoTransporte === undefined){
+        return res.status(400).json({error:"distancia e transporte obrigatorios"})
+    }
+
+    const precoPorKM = precos[tipoTransporte.toLowerCase()];
+    if(precoPorKM === undefined){
+        return res.status(400).json({error:"tipo de transporte invalido"})
+    }
+
+    // calcular o valor total do frete
+    const valorTotal = distancia * precoPorKM
+    res.json({valorTotal: valorTotal.toFixed(2)}) //ARREDONDA PARA 2 CASAS DECIMAIS
+    
+})
+
+
 app.listen(port,()=>{
     console.log(`Servidor rodando na porta ${port}`)
 })
